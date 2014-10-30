26c555ecb22fcd93245f685b80216da45cc85e51
lacaulac <antonin17@live.fr>
2014-10-30 13:36:35 +0100

    Added MLL main.js

---
+++ mll/main.js
@@ -0,0 +1,64 @@
+var username, action, param;
+function getVars1()
+{
+	username = document.forms["form"].elements["username"].value;
+	action = document.forms["form"].elements["action"].value;
+}
+function execute()
+{
+	getVars1();
+	var string1, string2, string3;
+	if(action == "mute")
+	{
+		string1 = "/setrole ";
+		string2 = username;
+		string3 = " listener";
+	}
+	else if(action == "unmute")
+	{
+		string1 = "/setrole ";
+		string2 = username;
+		string3 = " user";
+	}
+	else if(action == "kick")
+	{
+		string1 = "/kick ";
+		string2 = username;
+		string3 = "";
+	}
+	else if(action == "ajout")
+	{
+		string1 = "/add ";
+		string2 = username;
+		string3 = "";
+	}
+	document.getElementById("response1").value = string1 + string2 + string3;
+	document.getElementById("response1").focus();
+}
+function getVars2()
+{
+	username = document.getElementById("demandant").value;
+	action = document.forms["props"].elements["actions"].value;
+	param = document.getElementById("param").value;
+}
+function demande()
+{
+	getVars2();
+	var string1, string2, string3, string4;
+	if(action == "modo")
+	{
+		string1 = "*";
+		string2 = username;
+		string3 = " souhaite devenir modérateur(trice). !oui ou !non*";
+		string4 = "";
+	}
+	else if(action == "loi")
+	{
+		string1 = "*";
+		string2 = username;
+		string3 = " souhaite proposer une loi: \"";
+		string4 = param + "\". !oui ou !non*";
+	}
+	document.getElementById("response2").value = string1 + string2 + string3 + string4;
+	document.getElementById("response2").focus();
+}
