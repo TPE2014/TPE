5ac1a2e264d52ba244689236a80823a6f8db8f37
lacaulac <antonin17@live.fr>
2014-11-06 17:13:20 +0100

    Updated

--- chifumi.js
+++ chifumi.js
@@ -64,11 +64,11 @@ function chooseShot()//Here comes the AI
 			return response(3);
 		}
 	}
-	else if((lastVar(prev) == lastVar2(prev)) && isDefined(lastVar2(prev)) && bourinEngaged != 1)
+	else if((lastVar(prev) == lastVar2(prev)) && isDefined(lastVar2(prev)) && bourinEngaged != 2)
 	{
 	console.log("2");
 		bourinEngaged++;
-		if(rdm(1, 2) == 1)
+		if(rdm(1, 4) == 1)
 		{
 			return rdmShot();
 		}
