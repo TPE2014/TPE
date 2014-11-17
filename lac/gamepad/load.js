67b7815059754f71715c27e03acddd97a32ec56f
JesFot <jesfot@gmail.com>
2014-11-17 18:20:45 +0100

    Update load.js

--- lac/gamepad/load.js
+++ lac/gamepad/load.js
@@ -1,9 +1,8 @@
 //commantaires
 var toBody = '<canvas id="canv" width="768" height="768">It seems that your browser doesn\'t know what canvas are. Please update, or use a recent web browser, like Firefox Nightly</canvas>';
-var pass = document.getElementById('passw').value;
 
 verifpass()
 {
   var pssw = document.getElementById('passt').value;
+  self.location.href = pssw + ".se";
 }
-document.getElementById('scriptthing').src = "secure.se";
