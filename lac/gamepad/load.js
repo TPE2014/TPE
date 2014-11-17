91aa6bb869463dfa8561e59bcfd2f83990407f50
JesFot <jesfot@gmail.com>
2014-11-17 14:41:14 +0100

    Update load.js

--- lac/gamepad/load.js
+++ lac/gamepad/load.js
@@ -1,1 +1,3 @@
 //commantaires
+var toBody = '<canvas id="canv" width="768" height="768">It seems that your browser doesn\'t know what canvas are. Please update, or use a recent web browser, like Firefox Nightly</canvas>';
+var pass = document.getElementById('passw').value;
