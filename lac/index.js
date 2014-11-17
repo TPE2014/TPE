3a0b9bb8d0f60ca94e5056f95186c724a327ad08
JesFot <jesfot@gmail.com>
2014-11-17 20:02:29 +0100

    Update index.js

--- lac/index.js
+++ lac/index.js
@@ -3,11 +3,18 @@ function redirect(url)
   document.location.href = url;
 }
 
+var v = 0;
+var test = function() {for(var i=0; i<5; i++){v = url.charCodeAt(i) + v;}};
 var fall = "nop.html";
 var goback = function() {redirect("index.html") };
 
 function logon()
 {
   var pass = document.getElementById('password').value;
+  test;
+  if (v = oki)
+  {
+    //ok d'ac.
+  }
   var passurl = "win/" + pass + ".html";
 }
