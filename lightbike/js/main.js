a3c526d0781a09712d1f05f9a4d53cbed558deba
lacaulac <antonin17@live.fr>
2014-11-19 20:07:27 +0100

    Added main.js

---
+++ lightbike/js/main.js
@@ -0,0 +1,35 @@
+var Graphics = require("js/graphics");
+var Gamepad = require("js/gamepad");
+var Utilities = require("js/utilities");
+var interval;
+var context = document.getElementById("canv").getContext("2d");
+var text = 'Welcome to our lightbike game!';
+Graphics.setUp(context, "30pt Verdana", "left", "top", "black");
+Graphics.drawText(context, text, 0, 0);
+Gamepad.setUp(interval);
+function gameLoop(ctx)
+{
+	Graphics.setUp(ctx, "12pt Source Code Pro", "left", "top", "darkgreen");
+	if(navigator.getGamepads().length)
+	{
+		ctx.clearRect(0, 50, 768, 768);
+		var controllerIndex = 0;
+		for(var i2=0; i2!=navigator.getGamepads().length;i2++)
+		{
+			var tmp = Gamepad.getController(i2);
+			//console.log(tmp);
+			if(tmp)
+			{
+				//console.log(tmp.buttons.toString());
+				Graphics.drawText(ctx, "Controller " + controllerIndex + "(" + tmp.index + ")", 300*controllerIndex, 50);
+				for(var i=0;i<tmp.buttons.length;i++)
+				{
+					//console.log(i);
+					Graphics.drawText(ctx, "Button " + i + " state: " + Utilities.boolSConvert(tmp.buttons[i].pressed), 300*controllerIndex, (30*(i+1)+50));
+				}
+				controllerIndex++;
+			}
+		}
+		controllerIndex = 0;
+	}
+}
