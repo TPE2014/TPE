a4468d22fad2b50a2b2c4b919c011cdd7b99553b
lacaulac <antonin17@live.fr>
2014-11-19 20:08:16 +0100

    Added Gamepad module

---
+++ lightbike/js/gamepad.js
@@ -0,0 +1,17 @@
+var who = "world";
+
+exports.setUp = function(interval)
+{
+		window.addEventListener("gamepadconnected", function(e) {
+		console.log("Contrôleur n°%d connecté : %s. %d boutons, %d axes.",
+		e.gamepad.index, e.gamepad.id,
+		e.gamepad.buttons.length, e.gamepad.axes.length);
+		alert("Gamepad connected!");
+		interval = setInterval(function() { gameLoop(context) }, 10);
+});
+}
+exports.getController = function(index)
+{
+	var tmp = navigator.getGamepads()[index];
+	return tmp;
+}
