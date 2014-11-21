8280664813350c3352100298278f6328f029c113
JesFot <jesfot@gmail.com>
2014-11-21 20:43:27 +0100

    Update IA.js

--- temporary/IA_lightbike/IA.js
+++ temporary/IA_lightbike/IA.js
@@ -51,3 +51,50 @@ function AI1(map, data)
     setTable(theMap);
   }
 }
+
+function AI2(map, data)
+{
+	var Pia = data.ia.Nbp;
+	var exX = data.ia.pos.ex[0];
+	var exY = data.ia.pos.ex[1];
+	var x = getPlayer(map, Pia)[0];
+	var y = getPlayer(map, Pia)[1];
+	var dir = getDirection(exX, exY, x, y);
+	if (dir == "noDir")
+	{
+		AI1(map, data);
+	}
+	else if (dir == "down")
+	{
+		//y-1
+		if (testforWall(x, y-1, map) || testforWall(x, y-1, map) == "player")
+		{
+			var rightZone = calc(x, y, 'x', 1, map);
+			var leftZone = calc(x, y, 'x', -1, map);
+			if (rightZone == leftZone)
+			{
+				AI1(map, data); // droite ...
+			}
+			else if (rightZone > leftZone)
+			{
+				movePlayer("right", Pia, map);
+			}
+			else if (rightZone < leftZone)
+			{
+				movePlayer("left", Pia, map);
+			}
+			else
+			{
+				console.log("error: in function AI2() cannot compare " + rightZone + " and " + leftZone + " in IA.js");
+			}
+		}
+		else if (testforWall(x, y-1, map) == false)
+		{
+			movePlayer("down", Pia, map);
+		}
+		else
+		{
+			console.log("Maybe a player ?");
+		}
+	}
+}
