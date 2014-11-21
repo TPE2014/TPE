d2785dbeface77b199e22c0330389778cca24b25
JesFot <jesfot@gmail.com>
2014-11-21 09:25:21 +0100

    Update IA.js

--- temporary/IA_lightbike/IA.js
+++ temporary/IA_lightbike/IA.js
@@ -10,7 +10,6 @@ function AI1(map, data)
   var x = data.ia.x;
   var y = data.ia.y;
   var Pia = data.ia.Nbp;
-  setPlayer(x, y, map, Pia);
   if (testforWall(x+1, y, map))
   {
     if (testforWall(x, y+1, map))
