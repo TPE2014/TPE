831b00037547898f0d75356c3ff7afba768efbf3
JesFot <jesfot@gmail.com>
2014-11-21 09:15:57 +0100

    Update IA.js

--- temporary/IA_lightbike/IA.js
+++ temporary/IA_lightbike/IA.js
@@ -4,3 +4,40 @@ function main(Player, map)
 {
   //C
 }
+
+function AI1(map, data)
+{
+  var x = data.ia.x;
+  var y = data.ia.y;
+  var Pia = data.ia.Nbp;
+  setPlayer(x, y, map, Pia);
+  if (testforWall(x+1, y, map))
+  {
+    if (testforWall(x, y+1, map))
+    {
+      if (testforWall(x-1, y, map))
+      {
+        if (testforWall(x, y-1, map))
+        {
+          cannot_move();
+        }
+        else
+        {
+          up(Pia);
+        }
+      }
+      else
+      {
+        left(Pia);
+      }
+    }
+    else
+    {
+      down(Pia);
+    }
+  }
+  else
+  {
+    right(Pia);
+  }
+}
