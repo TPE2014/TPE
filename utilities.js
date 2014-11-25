fb01688bf9989ea436223f878a8e1b0198bf6310
JesFot <jesfot@gmail.com>
2014-11-25 08:54:45 +0100

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,20 +1,23 @@
 var who = "world";
+
 exports.boolSConvert = function(inp)
 {
-if(inp)
-{
-return "true";
+  if(inp)
+  {
+    return "true";
+  }
+  else
+  {
+    return "false";
+  }
 }
-else
-{
-return "false";
-}
-}
+
 exports.turnArrondi = function(inp)
 {
-return Math.round(inp);
+  return Math.round(inp);
 }
+
 exports.troncature = function(inp, dec)
 {
-return inp.toFixed(dec);
+  return inp.toFixed(dec);
 }
