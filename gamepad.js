24387baa62a97a1820a2a70ddb122063d8367e42
JesFot <jesfot@gmail.com>
2014-11-25 13:43:39 +0100

    Update gamepad.js

--- gamepad.js
+++ gamepad.js
@@ -1,16 +1,18 @@
 var who = "world";
+
 exports.setUp = function(interval)
 {
-window.addEventListener("gamepadconnected", function(e) {
-console.log("Contrôleur n°%d connecté : %s. %d boutons, %d axes.",
-e.gamepad.index, e.gamepad.id,
-e.gamepad.buttons.length, e.gamepad.axes.length);
-alert("Gamepad connected!");
-interval = setInterval(function() { gameLoop(context) }, 10);
+  window.addEventListener("gamepadconnected", function(e) {
+  console.log("Contrôleur n°%d connecté : %s. %d boutons, %d axes.",
+  e.gamepad.index, e.gamepad.id,
+  e.gamepad.buttons.length, e.gamepad.axes.length);
+  alert("Gamepad connected!");
+  interval = setInterval(function() { searchLoop() }, 10);
 });
 }
+
 exports.getController = function(index)
 {
-var tmp = navigator.getGamepads()[index];
-return tmp;
+  var tmp = navigator.getGamepads()[index];
+  return tmp;
 }
