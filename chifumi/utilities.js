0166e67bfe493dca43caa73dcbd11aaef9f7502d
JesFot <jesfot@gmail.com>
2014-11-27 16:30:30 +0100

    Create utilities.js

---
+++ chifumi/utilities.js
@@ -0,0 +1,23 @@
+var who = "world";
+
+exports.boolSConvert = function(inp)
+{
+  if(inp)
+  {
+    return "true";
+  }
+  else
+  {
+    return "false";
+  }
+}
+
+exports.turnArrondi = function(inp)
+{
+  return Math.round(inp);
+}
+
+exports.troncature = function(inp, dec)
+{
+  return inp.toFixed(dec);
+}
