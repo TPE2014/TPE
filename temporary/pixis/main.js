51451b63265371d90f378733816063837c33cb1f
JesFot <jesfot@gmail.com>
2014-12-18 16:52:07 +0100

    Update main.js

--- temporary/pixis/main.js
+++ temporary/pixis/main.js
@@ -1,1 +1,18 @@
-var save = {"sprite":{},"textures":{},"id":[]};
+var save = {"sprite":[],"textures":[],"id":{}};
+var degres = 15705;
+
+function newID(table)
+{
+  return table.length;
+}
+
+function add_image(link)
+{
+  var tmpID = newID(save.textures);
+  var newTexture = new PIXI.Texture.fromImage(link);
+  save.textures[tmpID] = {};
+  save.textures[tmpID].id = tmpID;
+  save.textures[tmpID].name = name;
+  save.textures[tmpID].data = newTexture;
+  return tmpID;
+}
